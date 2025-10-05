a9facfe269c0df9d3e0086546feaca042f7ea11f
razaumar3400-sketch <razaumar3400@gmail.com>
2025-10-05 01:17:38 -0700

    Update mykeywords.py

--- mykeywords.py
+++ mykeywords.py
@@ -12,13 +12,7 @@ days = st.number_input("Enter Days to Search (1-30):", min_value=1, max_value=30
 
 # List of broader keywords
 keywords = [
- "Affair Relationship Stories", "Reddit Update", "Reddit Relationship Advice", "Reddit Relationship", 
-"Reddit Cheating", "AITA Update", "Open Marriage", "Open Relationship", "X BF Caught", 
-"Stories Cheat", "X GF Reddit", "AskReddit Surviving Infidelity", "GurlCan Reddit", 
-"Cheating Story Actually Happened", "Cheating Story Real", "True Cheating Story", 
-"Reddit Cheating Story", "R/Surviving Infidelity", "Surviving Infidelity", 
-"Reddit Marriage", "Wife Cheated I Can't Forgive", "Reddit AP", "Exposed Wife", 
-"Cheat Exposed"
+ "Twisted Family Revenge,reddit,reddit story, reddit family drama sisters,reddit family drama stories, family drama stories, dramatic confessions, forgiveness stories, resilience and justice"
 ]
 
 # Fetch Data Button
